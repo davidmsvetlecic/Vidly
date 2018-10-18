8fa387b9f754cc0d5cede952ed9e12090a2eeb02
David Svetlecic <DSvetlecic@cylance.com>
2018-10-18 09:31:35 -0500

    Feed DataTable with customers API

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -8,28 +8,28 @@ namespace Vidly
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/bootbox.js",
-                        "~/Scripts/DataTables/jquery.dataTables.js",
-                        "~/Scripts/DataTables/dataTables.bootstrap.js"));
+                        "~/scripts/jquery-{version}.js",
+                        "~/scripts/bootstrap.js",
+                        "~/scripts/bootbox.js",
+                        "~/scripts/datatables/jquery.datatables.js",
+                        "~/scripts/datatables/datatables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootbox.js",
-                        "~/Scripts/bootstrap.js"));
+                        "~/scripts/bootbox.js",
+                        "~/scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/Content/bootstrap-lumen.css",
-                        "~/Content/DataTables/css/dataTables.bootstrap.css",
-                        "~/Content/site.css"));
+                        "~/content/bootstrap-lumen.css",
+                        "~/content/datatables/css/datatables.bootstrap.css",
+                        "~/content/site.css"));
         }
     }
 }
